213adff6b531e71b6b4263cfb70f0eaf4238a502
slawek367 <slawomir.bando@gmail.com>
2018-01-20 21:28:21 +0100

    Register html form ready

--- web/register.py
+++ web/register.py
@@ -1,15 +1,16 @@
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from flask import request
 
 class Register(Form):
 
     name = StringField('Name', [validators.Length(min=2, max=50)])
+    surrname = StringField('Surrname', [validators.Length(min=2, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Name', [validators.Length(min=6, max=50)])
 
     password = PasswordField('Password',[
         validators.DataRequired(),
         validators.EqualTo('confrim', message='Passwords do not match')
-
     ])
 
     confrim = PasswordField('Confrim Password')
