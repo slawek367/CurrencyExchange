8c3d64029eb28a41a0229b654686dc2f1a7403c5
slawek367 <slawomir.bando@gmail.com>
2018-01-24 00:00:31 +0100

    Added TODOs and deleted unused method

--- users.py
+++ users.py
@@ -4,7 +4,7 @@ from db import Db
 class Users():
 
     def __init__(self):
-        self.userList = {}
+        self.userList = {} #DELETE IT SOON
         self.userMaxId = 0
 
     @staticmethod
@@ -12,7 +12,7 @@ class Users():
         db = Db()
         db.queryInsert("INSERT INTO users(username, email, password, name, surrname) VALUES(%s, %s, %s, %s, %s)", (username, email, password, name, surrname))
 
-    def deleteUser(self, user):
+    def deleteUser(self, user): #TODO add mysql
         if user.id in self.userList:
             del self.userList[user.id]
             #self.userMaxId = self.userMaxId - 1
@@ -32,7 +32,7 @@ class Users():
 
         return userList;
 
-    def getUser(self, id = None, name = None, surrname = None, login = None):
+    def getUser(self, id = None, name = None, surrname = None, login = None): #TODO add mysql
         if not id == None:
             if id in self.userList:
                 return self.userList[id]
@@ -51,10 +51,7 @@ class Users():
 
         return False
 
-    def printUsers(self):
+    def printUsers(self): #TODO add mysql
         for key, value in self.userList.items():
             value.info()
 
-    def loadUsers(self):
-        pass
-        #TODO
